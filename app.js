ee0f7fe8b106b25921f5d53a2187983141c99b49
robmab <rob_mb@outlook.es>
2023-03-27 20:29:48 +0200

    "Domain hack"

---
+++ app.js
@@ -0,0 +1,33 @@
+const genDomain = () => {
+    const pronoun = ['the', 'our', 'your', 'their', 'my', 'this'];
+    const adj = ['glass', 'cold', 'nice', 'round', 'wonderful'];
+    const noun = ['dwelling', 'campus'];
+    const ext = ['io', 'co', 'dev', 'xyz', 'org', 'app', 'us']
+    let aux;
+    let aux2;
+
+    pronoun.forEach(a => {
+        adj.forEach(b => {
+            noun.forEach(c => {
+                ext.forEach(d => {
+
+                    console.log(a + b + c + '.' + d);
+                    document.write(a + b + c + '.' + d + "<br>");
+
+                    aux = c.split("").splice(c.length - d.length, d.length).join("");
+                    aux2 = c.split("").splice(0, c.length - d.length).join("");
+
+                    if (d === aux) {
+                        console.log(a + b + aux2 + '.' + d);
+                        document.write(a + b + aux2 + '.' + d + "<br>");
+                    }
+
+                });
+                console.log("\n");
+                document.write("<br>")
+            });
+        });
+    });
+}
+
+genDomain();
--- src/app.js
+++ src/app.js
@@ -1,11 +1,33 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+const genDomain = () => {
+  const pronoun = ['the', 'our', 'your', 'their', 'my', 'this'];
+  const adj = ['glass', 'cold', 'nice', 'round', 'wonderful'];
+  const noun = ['dwelling', 'campus'];
+  const ext = ['io', 'co', 'dev', 'xyz', 'org', 'app', 'us']
+  let aux;
+  let aux2;
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+  pronoun.forEach(a => {
+    adj.forEach(b => {
+      noun.forEach(c => {
+        ext.forEach(d => {
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+          console.log(a + b + c + '.' + d);
+          document.write(a + b + c + '.' + d + "<br>");
+
+          aux = c.split("").splice(c.length - d.length, d.length).join("");
+          aux2 = c.split("").splice(0, c.length - d.length).join("");
+
+          if (d === aux) {
+            console.log(a + b + aux2 + '.' + d);
+            document.write(a + b + aux2 + '.' + d + "<br>");
+          }
+
+        });
+        console.log("\n");
+        document.write("<br>")
+      });
+    });
+  });
+}
+
+genDomain();
